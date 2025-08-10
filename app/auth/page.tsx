ff4ad4f2bb82f8beaf2a026d34c826f7c28d85e8
Ahmad Akbar Mubarak <130969627+ahmakbardev@users.noreply.github.com>
2025-08-10 18:38:00 +0700

    feat: fix useSearchParams err

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,13 +1,16 @@
-// app/(auth)/page.tsx
 import AuthIllustration from './components/AuthIllustration';
 import AuthDrawer from './components/AuthDrawer';
 
-export default function AuthIndexPage() {
+export default function AuthIndexPage({ searchParams }: { searchParams: { mode?: string } }) {
+  const initialMode =
+    searchParams.mode === 'login' || searchParams.mode === 'register'
+      ? (searchParams.mode as 'login' | 'register')
+      : 'login';
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen max-w-[500px] w-full mx-auto px-4 pb-5">
       <AuthIllustration />
-      <AuthDrawer />
-      {/* <p className="text-xs text-orange-500 mt-6 underline">Looking for help?</p> */}
+      <AuthDrawer initialMode={initialMode} />
     </div>
   );
 }
